fc8d63f457249147fbc5ff5060fe254a2cec4027
Freedom Walker <109196117+LH-Programer@users.noreply.github.com>
2024-02-23 19:49:44 -0300

    Add files via upload

--- Js/gringo.js
+++ Js/gringo.js
@@ -1,4 +1,4 @@
-
+// convertor plata
 var dolar = document.getElementById("dolar");
 
 var peso = document.getElementById("peso");
@@ -8,10 +8,10 @@ const cambio = document.getElementById("cambio");
 dolar.addEventListener("input", function(){
 
     const inputValue = parseFloat(dolar.value);
-    const clp = 981 * inputValue;
+    const clp = 980.39 * inputValue;
 
     if (!isNaN(clp)) {
-        peso.value = clp.toFixed();
+        peso.value = clp.toPrecision();
     } else {
         peso.value = "";
     }
@@ -43,10 +43,10 @@ cambio.addEventListener('click', function toggleInput(){
 peso.addEventListener("input", function(){
 
     const inputValue = parseFloat(peso.value);
-    const us = 0.0012 * inputValue;
+    const us = 0.0011 * inputValue;
 
     if (!isNaN(us)) {
-        dolar.value = us.toFixed(2);
+        dolar.value = us.toPrecision(2);
     } else {
         dolar.value = "";
     }
@@ -54,4 +54,34 @@ peso.addEventListener("input", function(){
 });
 
 
+// convertor medidas
 
+var inch = document.getElementById("inch");
+
+var cm = document.getElementById("cm");
+
+inch.addEventListener("input", function(){
+
+    const inputValue2 = parseFloat(inch.value);
+    const cm2 = 2.54 * inputValue2;
+
+    if (!isNaN(cm2)) {
+        cm.value = cm2.toPrecision();
+    } else {
+        cm.value = "";
+    }
+
+});
+
+cm.addEventListener("input", function(){
+
+    const inputValue2 = parseFloat(cm.value);
+    const inches = 0.39370079 * inputValue2;
+
+    if (!isNaN(inches)) {
+        inch.value = inches.toPrecision();
+    } else {
+        inch.value = "";
+    }
+
+});
